ae87858c9aa4fdb9f28c212bbe4d6f3e8879ea7c
Zack Borges <zackartborges@gmail.com>
2021-07-07 01:02:06 -0400

    adds some of message-create method

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,5 +1,12 @@
 class Api::MessagesController < ApplicationController
-  def index
-    messages = Connection.messages
+  def create
+    @message = Message.new(
+      connection_id: params[:connection_id],
+      user_id: current_user.id,
+      body: params[:body]
+    )
+    if @message.save
+      render json: {message: @message}
+    end
   end
 end
