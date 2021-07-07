b8fb02f58c951c5df9fb86b1b5baf749c3735483
winstonbarbe <winston.barbe@gmail.com>
2021-07-06 22:59:08 -0400

    adds signup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_07_024047) do
+ActiveRecord::Schema.define(version: 2021_07_07_025056) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 2021_07_07_024047) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.string "name"
     t.string "image_url"
     t.text "bio"
