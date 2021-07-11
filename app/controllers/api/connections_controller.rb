fbb7554b9ce61279c2a09d8f2488a3a26183ad23
winstonbarbe <winston.barbe@gmail.com>
2021-07-11 13:29:33 -0400

    adds connection destroy action

--- app/controllers/api/connections_controller.rb
+++ app/controllers/api/connections_controller.rb
@@ -5,6 +5,22 @@ class Api::ConnectionsController < ApplicationController
     render "index.json.jb"
   end
 
+  def create
+    # Need to make it so default of status is set to zero
+    # Need to make sure duplicates can't happen
+    # Potentially we need to include the other users information when creating
+    @connection = Connection.new(
+      sender_id: current_user.id,
+      recipient_id: params[:recipient_id],
+      status: 0
+    )
+    if @connection.save
+      render "show.json.jb", status: 201
+    else
+      render json: { errors: @connections.errors }, status: 404
+    end
+  end
+
   def show
     @connection = current_user.connections.find_by(id: params[:id])
     render "show.json.jb"
@@ -19,5 +35,10 @@ class Api::ConnectionsController < ApplicationController
       render json: { errors: @connection.errors }, status: 404
     end
   end
-  
+
+  def destroy
+    connection = current_user.accepted_connections.find(params[:id])
+    render json: { message: "No longer connected to this user" }
+  end
+
 end
