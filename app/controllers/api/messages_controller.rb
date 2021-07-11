8c2c3545fe38b9a8f56738397d2d04f67e5e0f20
Zack Borges <zackartborges@gmail.com>
2021-07-11 11:40:27 -0400

    sets read to false on message create

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -3,7 +3,8 @@ class Api::MessagesController < ApplicationController
     @message = Message.new(
       connection_id: params[:connection_id],
       user_id: current_user.id,
-      body: params[:body]
+      body: params[:body],
+      read: false,
     )
     if @message.save
       render json: {message: @message}
