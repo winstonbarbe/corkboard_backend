466be084c6eb0ee9a24334d0ac396d5f516fe256
winstonbarbe <winston.barbe@gmail.com>
2021-07-20 15:40:53 -0400

    adds broadcast to message controller

--- app/channels/messages_channel.rb
+++ app/channels/messages_channel.rb
@@ -1,6 +1,6 @@
 class MessagesChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
+    stream_from "messages_channel"
   end
 
   def unsubscribed
--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -6,9 +6,29 @@ class Api::MessagesController < ApplicationController
       body: params[:body],
       read: false,
     )
-    if @message.save
-      render json: {message: @message}
-    end
+    @message.save
+
+    ActionCable.server.broadcast "messages_channel", {
+      {
+        id: @message.id,
+        body: @message.body,
+        read: @message.read,
+        sent: @message.created_at,
+        user: {
+          {
+            id: @message.user.id,
+            username: @message.user.username,
+            name: @message.user.name,
+            email: @message.user.email,
+            current_location: @message.user.current_location,
+            bio: @message.user.bio,
+            image_url: @message.user.image_url
+          }
+        }
+      }
+    }
+    
+    render json: {message: @message}
   end
 
 end
