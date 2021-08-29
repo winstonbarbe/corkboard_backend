ae04078c091e13bdb5d39af3020c41da66fb3bc6
Zack Borges <zackartborges@gmail.com>
2021-08-29 11:57:03 -0400

    uses user_params for update function

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,5 +1,6 @@
 class Api::SessionsController < ApplicationController
   def create
+    # binding.pry
     user = User.find_by(username: params[:username])
     if user && user.authenticate(params[:password])
       jwt = JWT.encode(
--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,5 +1,5 @@
 class Api::UsersController < ApplicationController
-  before_action :authenticate_user, except: :create
+  # before_action :authenticate_user, except: :create
 
   def index
     @users = current_user.potential_connections
@@ -28,15 +28,9 @@ class Api::UsersController < ApplicationController
   end
   
   def update
+    # binding.pry
     @user = current_user
-    @user.name = params[:name] || @user.name
-    @user.username = params[:username] || @user.username
-    @user.email = params[:email] || @user.email
-    @user.bio = params[:bio] || @user.bio
-    @user.current_location = params[:current_location] || @user.current_location
-    @user.image_url = params[:image_url] || @user.image_url
-    @user.active = params[:active] || @user.active
-    if @user.save
+    if @user.update(user_params)
       render "show.json.jb", status: 200
     else
       render json: { errors: @user.errors }, status: 400
@@ -46,5 +40,11 @@ class Api::UsersController < ApplicationController
   def destroy
     current_user.delete
     render json: { message: "This user has been deleted" }, status: 201
+  end
+
+  private
+
+  def user_params
+    params.permit(:name, :username, :email, :bio, :current_location, :image_url, :active)
   end
 end
