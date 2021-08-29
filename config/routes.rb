5a18579313fc24780cdf148cc9aa143d5aeeb598
Zack Borges <zackartborges@gmail.com>
2021-08-29 12:09:25 -0400

    Update frontend files

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
     resources :users, :sessions, :connections, :messages
+
+
+  get "/*path" => proc { [200, {}, [ActionView::Base.new.render(file: 'public/index.html')]] } 
+
+
   end
 end
